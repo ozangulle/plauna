d6f907fcc38410f217b3f774d9e3c748ce2d7161
Ozan Gulle <ozan@ozangulle.xyz>
2025-07-01 21:29:43 +0200

    Add forgotten migration

--- src/plauna/entry.clj
+++ src/plauna/entry.clj
@@ -33,8 +33,6 @@
         (client/schedule-health-checks)))
   (t/log! :debug "Listening to new emails from listen-channel"))
 
-(def args nil)
-
 (defn -main
   [& args]
   (setup-logging)
@@ -43,7 +41,7 @@
     (database/create-db)
     (t/log! :info "Setting log level according to preferences.")
     (t/set-min-level! (preferences/log-level))
-    ;(start-imap-client application-config)
+    (start-imap-client application-config)
     (events/start-event-loops event-register)
     (server/start-server application-config)))
 
