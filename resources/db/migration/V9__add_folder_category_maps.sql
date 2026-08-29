cb00131863f958054aa08bbf7873b515ee67c2f8
Ozan Gulle <ozan@ozangulle.xyz>
2026-08-29 20:57:58 +0200

    add uniqueness clause to migration

--- resources/db/migration/V9__add_folder_category_maps.sql
+++ resources/db/migration/V9__add_folder_category_maps.sql
@@ -2,5 +2,7 @@ CREATE TABLE folder_category_maps(
        id INTEGER PRIMARY KEY AUTOINCREMENT,
        connection_id REFERENCES connections(id) ON DELETE CASCADE,
        folder TEXT,
-       category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE
+       category_id INTEGER REFERENCES categories(id) ON DELETE CASCADE,
+
+       UNIQUE (connection_id, folder, category_id)
 );
